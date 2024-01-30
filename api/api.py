8c66c9f7de963a5a40f8939b57e20eea46e2c8d1
Jonathan Rozario <jonathanrozario@gmail.com>
2024-01-29 21:02:16 -0500

    chore: Use namespace

--- api/api.py
+++ api/api.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/time")
+@app.route("/api/time")
 def get_current_time():
     current_time = datetime.now(tz=UTC)
     return {"time": current_time.isoformat("T", "seconds")}
